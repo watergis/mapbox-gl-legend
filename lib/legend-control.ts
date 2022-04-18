810b7b62e13422a94701ace73d92f7cca924f9bd
Jin Igarashi <igarashi.jin@water-gis.com>
2022-04-18 20:52:11 +0100

    fixed legend

--- lib/legend-control.ts
+++ lib/legend-control.ts
@@ -150,6 +150,7 @@ export default class MapboxLegendControl implements IControl
                 'http://www.w3.org/2000/svg',
                 'path'
             );
+            const iconPath2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 
             iconSvg.setAttribute('fill', 'none');
             iconSvg.setAttribute('viewBox', '0 0 24 24');
@@ -157,14 +158,24 @@ export default class MapboxLegendControl implements IControl
             iconSvg.classList.add('post-icon');
 
             iconPath.setAttribute(
-                'd',
-                'M21,0H3A3,3,0,0,0,0,3V21a3,3,0,0,0,3,3H21a3,3,0,0,0,3-3V3A3,3,0,0,0,21,0ZM3,2H21a1,1,0,0,1,1,1V15.86L14.18,9.35a5.06,5.06,0,0,0-6.39-.06L2,13.92V3A1,1,0,0,1,3,2ZM21,22H3a1,1,0,0,1-1-1V16.48l7-5.63a3.06,3.06,0,0,1,3.86,0L22,18.47V21A1,1,0,0,1,21,22Z"/><path d="M18,9a3,3,0,1,0-3-3A3,3,0,0,0,18,9Zm0-4a1,1,0,1,1-1,1A1,1,0,0,1,18,5Z'
-            );
-            iconPath.setAttribute('stroke-linecap', 'round');
-            iconPath.setAttribute('stroke-linejoin', 'round');
-            iconPath.setAttribute('stroke-width', '2');
+				'd',
+				'M21,0H3A3,3,0,0,0,0,3V21a3,3,0,0,0,3,3H21a3,3,0,0,0,3-3V3A3,3,0,0,0,21,0ZM3,2H21a1,1,0,0,1,1,1V15.86L14.18,9.35a5.06,5.06,0,0,0-6.39-.06L2,13.92V3A1,1,0,0,1,3,2ZM21,22H3a1,1,0,0,1-1-1V16.48l7-5.63a3.06,3.06,0,0,1,3.86,0L22,18.47V21A1,1,0,0,1,21,22Z'
+			);
+			
+			iconPath.setAttribute('stroke-linecap', 'round');
+			iconPath.setAttribute('stroke-linejoin', 'round');
+			iconPath.setAttribute('stroke-width', '2');
 
-            iconSvg.appendChild(iconPath);
+			iconPath2.setAttribute(
+				'd',
+				'M18,9a3,3,0,1,0-3-3A3,3,0,0,0,18,9Zm0-4a1,1,0,1,1-1,1A1,1,0,0,1,18,5Z'
+			);
+			iconPath2.setAttribute('stroke-linecap', 'round');
+			iconPath2.setAttribute('stroke-linejoin', 'round');
+			iconPath2.setAttribute('stroke-width', '2');
+
+			iconSvg.appendChild(iconPath);
+			iconSvg.appendChild(iconPath2);
 
             var label2 = document.createElement('label');
             label2.textContent = (this.targets && this.targets[layer.id])?this.targets[layer.id]:layer.id;
